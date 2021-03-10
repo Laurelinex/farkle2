fa0d15317e2672ebbd465204ae231deeb5b13a0b
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-10 18:34:08 +0000

    landing page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          
+          <Route path="/" exact>
+            <LandingPage />
+          </Route>
         </Switch>
       </Router>
 
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,14 @@
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Farkle</h1>
+            <button>1p</button>
+            <button>2p</button>
+        </div>
+        
+    )
+   
+}
+
+export default LandingPage;
